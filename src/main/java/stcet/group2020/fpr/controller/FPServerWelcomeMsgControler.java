95b075ba03d08c229487377e53ee6c1c7e0f3801
Supravo <supravosengupta1@gmail.com>
2020-03-12 20:01:23 +0530

    Welcome message controler added.

---
+++ src/main/java/stcet/group2020/fpr/controller/FPServerWelcomeMsgControler.java
@@ -0,0 +1,16 @@
+package stcet.group2020.fpr.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/fpserver")
+public class FPServerWelcomeMsgControler {
+    @GetMapping("/welcomemsg")
+    public String getAdmin() {
+        return "Wellcome From FP Server.";
+    }
+}
