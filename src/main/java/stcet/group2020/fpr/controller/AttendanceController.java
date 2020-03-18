15a1a06d3aa498e9880b2c893da8d19e3b58ccc7
thenath1998 <rkz.suman@gmail.com>
2020-03-18 21:52:21 +0530

    attendance updated: local

--- src/main/java/stcet/group2020/fpr/controller/AttendanceController.java
+++ src/main/java/stcet/group2020/fpr/controller/AttendanceController.java
@@ -2,7 +2,7 @@ package stcet.group2020.fpr.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +17,7 @@ public class AttendanceController {
 	@Autowired
 	private AttendanceRepository attendanceRepository;
 
-	@GetMapping(params = {"regNo", "classId"})
+	@PutMapping(params = {"regNo", "classId"})
 	public int addAttendance(@RequestParam("regNo") String regNo, @RequestParam("classId") long classId){
 		return attendanceRepository.addAttendance(regNo, classId);
 	}
