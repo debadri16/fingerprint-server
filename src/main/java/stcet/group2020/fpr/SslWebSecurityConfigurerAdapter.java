a08cdd73fd1b67076b050fdf10c4f42c5764f2e8
thenath1998 <rkz.suman@gmail.com>
2020-03-24 22:04:06 +0530

    https

---
+++ src/main/java/stcet/group2020/fpr/SslWebSecurityConfigurerAdapter.java
@@ -0,0 +1,16 @@
+package stcet.group2020.fpr;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration(proxyBeanMethods = false)
+public class SslWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Customize the application security
+        http.requiresChannel().anyRequest().requiresSecure();
+    }
+
+}
