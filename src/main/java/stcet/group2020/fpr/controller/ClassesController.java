99f9ed0377bf0e995ae07cdae174e954c45aaec1
thenath1998 <rkz.suman@gmail.com>
2020-03-25 15:38:23 +0530

    delete class

--- src/main/java/stcet/group2020/fpr/controller/ClassesController.java
+++ src/main/java/stcet/group2020/fpr/controller/ClassesController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -89,5 +90,10 @@ public class ClassesController {
 		attendanceRepository.saveAll(attendances);
 
 		return classResponse;
+	}
+
+	@DeleteMapping(params = "classId")
+	public void deleteClass(@RequestParam("classId") Long classId){
+		classesRepository.deleteById(classId);
 	}
 }
